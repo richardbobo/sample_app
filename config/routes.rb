a1cbf862dc6e58f604c9619218b0f2e6a7fa6ce2
liang bo <kxo@hotmail.com>
2016-12-04 15:25:01 +0800

    finish static pages

--- config/routes.rb
+++ config/routes.rb
@@ -3,6 +3,8 @@ Rails.application.routes.draw do
 
   get 'staticpages/help'
 
+get 'staticpages/about'
+get 'staticpages/contact'
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
-root 'application#hello'
+root 'staticpages#home'
 end
