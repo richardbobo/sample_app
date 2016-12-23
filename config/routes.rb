ab8e52544c350fa97dc02c59570a622c07947ea6
liang bo <kxo@hotmail.com>
2016-12-23 22:04:56 +0800

    finish user signup

--- config/routes.rb
+++ config/routes.rb
@@ -1,9 +1,10 @@
 Rails.application.routes.draw do
-  get '/signup',to:'user#new'
 
   root 'staticpages#home'
   get '/help',  to:'staticpages#help'
   get '/about',  to:'staticpages#about'
   get '/contact',to:'staticpages#contact'
+  get  '/signup',  to: 'users#new'
+  resources :users  
 end
 
